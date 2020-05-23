2aaa6f46f78a3472337c426ddc803c21d833f2bc
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-05-23 19:59:56 +0530

    Add support for multiple orders

--- frontend/src/MakeRequest.js
+++ frontend/src/MakeRequest.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
-import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
@@ -15,11 +15,25 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function MakeRequest() {
+  const [items, setItem] = useState([1]);
+  const [count, setCount] = useState(1);
+
   const classes = useStyles();
+
+  const handleInsert = () => {
+    setCount((prevCount) => prevCount + 1);
+    const newItems = [...items, count];
+    setItem(newItems);
+  };
+
+  const handleDelete = (id) => {
+    const newItems = items.filter((itemId) => id !== itemId);
+    setItem(newItems);
+  };
 
   return (
     <Box m={5}>
-      <Box fontSize="h4.fontSize" fontWeight="fontWeightLight" mb={2} ml={1}>
+      <Box fontSize="h4.fontSize" fontWeight="fontWeightLight" mb={4} ml={1}>
         Place your request
       </Box>
       <form>
@@ -42,42 +56,66 @@ export default function MakeRequest() {
             />
           </Grid>
         </Grid>
-        <Grid container spacing={2} alignItems="center">
-          <Grid item sm={5} xs={11}>
-            <TextField
-              required
-              fullWidth
-              label="Item"
-              variant="outlined"
-              className={classes.textField}
-            />
+        {items.map((elem) => (
+          <Grid container spacing={2} alignItems="center" key={elem}>
+            <Grid item sm={5} xs={11}>
+              <TextField
+                required
+                fullWidth
+                label="Item"
+                variant="outlined"
+                className={classes.textField}
+              />
+            </Grid>
+            <Grid item sm={2} xs={5}>
+              <TextField
+                required
+                fullWidth
+                label="Shop"
+                variant="outlined"
+                className={classes.textField}
+              />
+            </Grid>
+            <Grid item sm={2} xs={4}>
+              <TextField
+                required
+                fullWidth
+                type="number"
+                label="Qty"
+                variant="outlined"
+                className={classes.textField}
+              />
+            </Grid>
+            <Grid item sm={1} xs={1}>
+              <IconButton
+                size="large"
+                disabled={items.length === 1}
+                onClick={() => handleDelete(elem)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </Grid>
           </Grid>
-          <Grid item sm={2} xs={6}>
-            <TextField
-              required
-              fullWidth
-              label="Shop"
-              variant="outlined"
+        ))}
+        <Grid container spacing={2}>
+          <Grid item>
+            <Button
               className={classes.textField}
-            />
+              variant="contained"
+              size="large"
+            >
+              Request
+            </Button>
           </Grid>
-          <Grid item sm={2} xs={5}>
-            <TextField
-              required
-              fullWidth
-              type="number"
-              label="Qty"
-              variant="outlined"
+          <Grid item>
+            <Button
               className={classes.textField}
-            />
-          </Grid>
-          <Grid item sm={2}>
-            <IconButton size="large">
-              <AddIcon />
-            </IconButton>
-            <IconButton size="large">
-              <DeleteIcon />
-            </IconButton>
+              variant="contained"
+              size="large"
+              onClick={handleInsert}
+            >
+              Add Item
+            </Button>
           </Grid>
         </Grid>
       </form>
